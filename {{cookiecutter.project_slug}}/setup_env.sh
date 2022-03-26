26fc70019950395d93eee1215c7b0761756798e6
Dhruvesh <dhruveshpate@umass.edu>
2022-03-26 14:42:11 -0400

    Added setup_env.sh

---
+++ {{cookiecutter.project_slug}}/setup_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+echo "=======Creating virtual env========="
+python3 -m virtualenv  .venv_{{project_slug}}
+source .venv_{{project_slug}}/bin/activate
+
+echo "=======Install test requirements======="
+pip install -r test_requirements.txt
+
+echo "=======Install doc requirements======="
+pip install -r doc_requirements.txt
+
+echo "=======Install core requirements======"
+pip install -r core_requirements.txt
+
+echo "========Create log dir for single runs========="
+mkdir -p logs
+
+echo "=======Login to wandb (optional)==============="
+wandb init
+
+echo "Do 'source .venv_{{project_slug}}/bin/activate' to load the enviroment."
