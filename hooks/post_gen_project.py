fad24b69f6b8e9761810e328e127fc52b13a58b8
Dhruvesh <dhruveshpate@umass.edu>
2023-08-15 13:11:25 -0400

    Made the defaul instantiation light weight

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,11 @@ import sys
 REMOVE_PATHS = [
     '{% if cookiecutter.use_vscode != "y" %} .vscode, {% endif %}',
     '{% if cookiecutter.use_umass_clusters != "y" %} .vscode/sftp.json, {% endif %}'
+    '{% if cookiecutter.use_nox != "y" %} noxfile.py, {% endif %}',
+    '{% if cookiecutter.use_pre_commit != "y" %} .pre-commit-config.yaml, {% endif %}',
+    '{% if cookiecutter.use_pytest != "y" %} tests, {% endif %}',
+    '{% if cookiecutter.use_github_actions != "y" %} .github, CHANGELOG.md, CONTRIBUTING.md {% endif %}',
+    '{% if cookiecutter.use_docs != "y" %} docs, doc_requirements.txt {% endif %}',
 ]
 
 for path in REMOVE_PATHS:
