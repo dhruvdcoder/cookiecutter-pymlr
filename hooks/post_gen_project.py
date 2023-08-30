546eb7d9b495920a6db2c1467650d6ba30036fc8
Dhruvesh <dhruveshpate@umass.edu>
2023-08-30 11:38:48 -0400

    Fix post hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,24 +1,28 @@
 import os
-import sys
+import shutil
 
 """Remove conditional files and folders after creation"""
 
-REMOVE_PATHS = [
-    '{% if cookiecutter.use_vscode != "y" %} .vscode, {% endif %}',
+REMOVE_PATHS = (
+    '{% if cookiecutter.use_vscode != "y" %} .vscode, {% endif %}'
     '{% if cookiecutter.use_umass_clusters != "y" %} .vscode/sftp.json, {% endif %}'
-    '{% if cookiecutter.use_nox != "y" %} noxfile.py, {% endif %}',
-    '{% if cookiecutter.use_pre_commit != "y" %} .pre-commit-config.yaml, {% endif %}',
-    '{% if cookiecutter.use_pytest != "y" %} tests, {% endif %}',
-    '{% if cookiecutter.use_github_actions != "y" %} .github, CHANGELOG.md, CONTRIBUTING.md {% endif %}',
-    '{% if cookiecutter.use_docs != "y" %} docs, doc_requirements.txt {% endif %}',
-]
+    '{% if cookiecutter.use_nox != "y" %} noxfile.py, {% endif %}'
+    '{% if cookiecutter.use_pre_commit != "y" %} .pre-commit-config.yaml, {% endif %}'
+    '{% if cookiecutter.use_pytest != "y" %} tests, {% endif %}'
+    '{% if cookiecutter.use_github_actions != "y" %} .github, CHANGELOG.md, CONTRIBUTING.md, {% endif %}'
+    '{% if cookiecutter.use_docs != "y" %} docs, doc_requirements.txt {% endif %}'
+)
 
-for path in REMOVE_PATHS:
+print("Removing conditional files and folders after creation")
+print(f"REMOVE_PATHS: {REMOVE_PATHS}")
+for path in REMOVE_PATHS.split(","):
     path = path.strip()
     if path and os.path.exists(path):
         print(f"Removing directory {path}")
         if os.path.isdir(path):
-            os.rmdir(path)
+            shutil.rmtree(path)
         else:
             print(f"Removing file {path}")
             os.unlink(path)
+    else:
+        print(f"Path {path} does not exist")
